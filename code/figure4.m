96d5549c4a65e4d7e009f261fcf5ff6724de77ba
KendricOrtiz <59103457+KendricOrtiz@users.noreply.github.com>
2020-10-30 12:05:11 -0600

    Updated figure4 and its function
    Included fucntions and data folder for figure4. Also made updates to figure4.m

--- code/figure4.m
+++ code/figure4.m
@@ -1,14 +1,14 @@
 %% Kernel Embeddings Example (First-Hitting Time Problem)
 % Kernel embeddings example showing the first-hitting time problem
-% for a planar quadcopter system.
+% for a 6-D quadcopter system.
 %
 %%
 % Specify the time horizon, the safe set $\mathcal{K}$, and the target set
 % $\mathcal{T}$.
 
-N = 5;
-K = srt.Tube(N, Polyhedron('lb', [-1 -100 0 -100 -100 -100], 'ub', [1 100 0.8 100 100 100]));
-T = srt.Tube(N, Polyhedron('lb', [-1 -100 -100 -100 -100 -100], 'ub', [1 100 0.8 100 100 100]));
+params.N = 5;
+K = srt.Tube(params.N, Polyhedron('lb', [-1 -500 0 -500 -500 -500], 'ub', [1 500 0.8 500 500 500]));
+T = srt.Tube(params.N, Polyhedron('lb', [-1 -500 0.8 -500 -500 -500], 'ub', [1 500 500 500 500 500]));
 
 problem = srt.problems.FirstHitting('ConstraintTube', K, 'TargetTube', T);
 
@@ -16,31 +16,33 @@ problem = srt.problems.FirstHitting('ConstraintTube', K, 'TargetTube', T);
 % Generate input/output samples for the Planar Quadrotor system
 %
 
-Ts = 0.25;
+% load('.\data\figure4samples.mat');
+tic
+params.Ts = 0.25;
 X_d = [0 0 1 0 0 0]';
-Xmin = -1.1;
-Xmax = 1.1;
-dXmin = 0;
-dXmax = -0.1;
-Ymin = -0.1;
-Ymax = 1;
-dYmin = -0.1;
-dYmax = 0.1;
-Tmin = -pi;
-Tmax = pi;
-dTmin = -0.1;
-dTmax = 0.1;
-el = [10, 3, 10, 3, 3, 2];
-Umin = -1;
-Umax = 1;
+params.Xmin = -1.5;
+params.Xmax = 1.5;
+params.dXmin = 0;
+params.dXmax = -0.1;
+params.Ymin = -0.3;
+params.Ymax = 1.2;
+params.dYmin = -0.1;
+params.dYmax = 0.1;
+params.Tmin = -pi;
+params.Tmax = pi;
+params.dTmin = -0.1;
+params.dTmax = 0.1;
+params.el = [10, 3, 10, 3, 3, 2];
+params.Umin = -1;
+params.Umax = 1;
 dtype = 1;
 
-X = linspace(Xmin, Xmax, el(1));
-dX = linspace(dXmin, dXmax, el(2));
-Y = linspace(Ymin, Ymax, el(3));
-dY = linspace(dYmin, dYmax, el(4));
-T = linspace(Tmin, Tmax, el(5));
-dT = linspace(dTmin, dTmax, el(6));
+X = linspace(params.Xmin, params.Xmax, params.el(1));
+dX = linspace(params.dXmin, params.dXmax, params.el(2));
+Y = linspace(params.Ymin, params.Ymax, params.el(3));
+dY = linspace(params.dYmin, params.dYmax, params.el(4));
+T = linspace(params.Tmin, params.Tmax, params.el(5));
+dT = linspace(params.dTmin, params.dTmax, params.el(6));
 
 [dTdT, TT, dYdY, YY, dXdX, XX] = ndgrid(dT, T, dY, Y, dX, X);
 
@@ -53,26 +55,19 @@ dT = reshape(dTdT, 1, []);
 
 Xs = [X; dX; Y; dY; T; dT];
 
-% Q = 10*eye(size(A,2)*N);
-% R = 10*eye(size(B,2)*N);
-% K = [   -0.3370   -0.6738    0.6396    1.8992    4.9544    2.0982;
-%          0.3370    0.6738    0.6396    1.8992   -4.9544   -2.0982;];
-% U = K*(x0-x_d);
-
 for k = 1:size(Xs,2)
-    Us(:,k) = quadInputGen(60,Ts,Xs(:,k),X_d);
+    Us(:,k) = quadInputGen(60,params.Ts,params.Umax,Xs(:,k),X_d);
 end
 
 Us2 = 0.01*Us(1:2,:);
-Ys = generate_output_samples_quad(Xs,Us2,Ts,1);
-args1 = {[6 1], 'X', Xs, 'U', Us2, 'Y', Ys};
+toc
+Ys1 = generate_output_samples_quad(params,1);
 
-sys1 = srt.systems.SampledSystem('X', Xs, 'U', Us2, 'Y', Ys);
+sys1 = srt.systems.SampledSystem('X', Xs, 'U', Us2, 'Y', Ys1);
 
-Ys = generate_output_samples_quad(Xs,Us2,Ts,2);
-args2 = {[6 1], 'X', Xs, 'U', Us2, 'Y', Ys};
+Ys2 = generate_output_samples_quad(params,2);
 
-sys2 = srt.systems.SampledSystem('X', Xs, 'U', Us2, 'Y', Ys);
+sys2 = srt.systems.SampledSystem('X', Xs, 'U', Us2, 'Y', Ys2);
 
 x = linspace(-1.1, 1.1, 100);
 y = linspace(0, 1, 100);
@@ -92,20 +87,58 @@ alg = srt.algorithms.KernelEmbeddings('sigma', 0.1, 'lambda', 1);
 %%
 % Call the algorithm.
 
+% Generate test points.
+% s = linspace(-1, 1, 100);
+% [X1, X2] = meshgrid(s);
 s = linspace(-1, 1, 100);
-Xt = sampleunif(s, s);
-Ut = zeros(1, size(Xt, 2));
+x = linspace(-1.1, 1.1, 100);
+y = linspace(0, 1, 100);
+z = zeros(1, 100^2);
+[xx, yy] = meshgrid(x, y);
+xx = reshape(xx, 1, []);
+yy = reshape(yy, 1, []);
+Xt = [xx; z; yy; z; z; z];
+Ut = zeros(2,size(Xt,2));
 
-resultsGuass = SReachPoint(problem, alg, sys1, Xt, Ut);
+% Xt = sampleunif(s, s);
+% Ut = zeros(1, size(Xt, 2));
+
+resultsGauss = SReachPoint(problem, alg, sys1, Xt, Ut);
 
 resultsBeta = SReachPoint(problem, alg, sys2, Xt, Ut);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
-% View the results.
-surf(s, s, reshape(resultsGuass(1, :), 100, 100), 'EdgeColor', 'none');
-surf(s, s, reshape(resultsBeta(1, :), 100, 100), 'EdgeColor', 'none');
+%% Plot the results.
+width = 80;
+height = 137;
 
+figure('Units', 'points', ...
+       'Position', [0, 0, 243, 172])
 
-% Save the figure as 'figure4'.
-saveas(gcf, '../results/figure4.png')
+ax1 = subplot(1, 2, 1, 'Units', 'points');
+surf(ax1, x, y, reshape(resultsGauss.Pr(params.N-1, :), 100, 100), 'EdgeColor', 'none');
+view([0 90]);
+axis([-1.1 1.1 0.5 1])
+
+colorbar(ax1, 'off');
+ax1.Position = [30, 25, width, 137];
+ax1.XLabel.Interpreter = 'latex';
+ax1.XLabel.String = '$z_{1}$';
+ax1.YLabel.Interpreter = 'latex';
+ax1.YLabel.String = '$z_{2}$';
+set(ax1, 'FontSize', 8);
+
+ax2 = subplot(1, 2, 2, 'Units', 'points');
+surf(ax2, x, y, reshape(resultsBeta.Pr(params.N-1, :), 100, 100), 'EdgeColor', 'none');
+view([0 90]);
+axis([-1.1 1.1 0.5 1])
+
+colorbar(ax2);
+ax2.YAxis.Visible = 'off';
+ax2.Position = [30 + 90, 25, width, 137];
+ax2.XLabel.Interpreter = 'latex';
+ax2.XLabel.String = '$z_{1}$';
+ax2.YLabel.Interpreter = 'latex';
+ax2.YLabel.String = '$z_{2}$';
+set(ax2, 'FontSize', 8);
--- code/quadInputGen.m
+++ code/quadInputGen.m
@@ -21,36 +21,40 @@ B = [0     0;
      0     0;
      r/I -r/I;];
  
-sys = ss(A,B,eye(1,size(A,2)),0);
-
-sysd = c2d(sys,Ts);
-
-A = sysd.A;
-B = sysd.B;
-         
-Q = 10*eye(size(A,2)*N);
-R = 1*eye(size(B,2)*N);
-X_d = repmat(x_d,[N 1]); 
-
-cvx_begin
-
-variable X(size(A,2)*N,)
-variable U(size(B,2)*N,)
-
-minimize((X-X_d)'*Q*(X-X_d) + U'*R*U)
-
-subject to
-
-    X(1:size(A,2)) == x0;
-
-    for k = 1:(N-1)
-        
-        X(size(A,2)*k+1:size(A,2)*(k+1)) == ...
-            A*X(size(A,2)*(k-1)+1:size(A,2)*(k)) + B*U(size(B,2)*(k-1)+1:size(B,2)*(k)); 
-
-    end
-    
-    abs(U) <= ulim; 
-    
-cvx_end
+K = [   -0.3370   -0.6738    0.6396    1.8992    4.9544    2.0982;
+         0.3370    0.6738    0.6396    1.8992   -4.9544   -2.0982;];
+U = K*(x0-x_d);
+ 
+% sys = ss(A,B,eye(1,size(A,2)),0);
+% 
+% sysd = c2d(sys,Ts);
+% 
+% A = sysd.A;
+% B = sysd.B;
+%          
+% Q = 10*eye(size(A,2)*N);
+% R = 1*eye(size(B,2)*N);
+% X_d = repmat(x_d,[N 1]); 
+% 
+% cvx_begin quiet
+% 
+% variable X(size(A,2)*N,)
+% variable U(size(B,2)*N,)
+% 
+% minimize((X-X_d)'*Q*(X-X_d) + U'*R*U)
+% 
+% subject to
+% 
+%     X(1:size(A,2)) == x0;
+% 
+%     for k = 1:(N-1)
+%         
+%         X(size(A,2)*k+1:size(A,2)*(k+1)) == ...
+%             A*X(size(A,2)*(k-1)+1:size(A,2)*(k)) + B*U(size(B,2)*(k-1)+1:size(B,2)*(k)); 
+% 
+%     end
+%     
+%     abs(U) <= ulim; 
+%     
+% cvx_end
 end
